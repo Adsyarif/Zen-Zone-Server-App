a0062030234f2a8fea991bc49e491738884c9669
Adsyarif <adrisansyarif@gmail.com>
2024-08-04 00:18:05 +0700

    feat: setup connectors to database

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from app.connector.sql_connector import connection
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
---
+++ app/connector/sql_connector.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import os
+
+DATABASE_TYPE = os.getenv('TYPE')
+DATABASE_HOST = os.getenv('HOST_DB')
+DATABASE_NAME = os.getenv('NAME_DB')
+DATABASE_PORT = os.getenv('PORT')
+DATABASE_USER = os.getenv('USER')
+DATABASE_PASSWORD = os.getenv('PASSWORD')
+
+print('Connecting to SQL Server')
+
+
+connection_string = f'{DATABASE_TYPE}://{DATABASE_USER}:{DATABASE_PASSWORD}@{
+    DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}'
+
+engine = create_engine(connection_string)
+
+connection = engine.connect()
+Session = sessionmaker(connection)
+
+print(f'connected to sql database {DATABASE_HOST}')
