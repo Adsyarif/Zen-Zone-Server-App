651f1be2bf4ac5256c8ddd54a336bac29ffe7b30
Abk1708 <bgus1708@gmail.com>
2024-08-10 15:20:31 +0700

    feat: add like and bookmark API functionality

--- app/controler/bookmark_controler.py
+++ app/controler/bookmark_controler.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
 from app.models.bookmarks import Bookmarks
+from app.models.posts import Posts
 from app.connector.sql_connector import Session
 from app.utils.api_response import api_response
 
@@ -13,3 +14,71 @@ def get_all_bookmarks():
         return api_response(status_code=500, message=f"Server error: {e}", data={})
     finally:
         session.close()
+        
+def do_bookmark_post(user_id, post_id):
+    session = Session()
+    try:
+        # data = request.json
+        
+        post_query = session.query(Posts).filter(Posts.user_id == user_id)
+        if not post_query:
+            return jsonify({'message': 'No related post found'}), 400
+        
+        bookmark_query = session.query(Bookmarks).filter(Bookmarks.user_id == user_id, Bookmarks.post_id == post_id).first()
+        if bookmark_query:
+            return jsonify({'message': 'Post already bookmarked by the user'}), 400
+        
+        add_bookmark = Bookmarks(
+            user_id=user_id,
+            post_id=post_id
+        )
+        
+        # if data.get('')
+        
+        session.add(add_bookmark)
+        session.commit()
+        
+        # data = add_like.serialize()
+        
+        return api_response(
+            status_code=200,
+            message="Post successfully bookmarked",
+            data=add_bookmark.serialize(full=False)
+        )
+    except Exception as e:
+        session.rollback()
+        print(f"Error occurred: {e}")
+        return api_response(
+            status_code=500,
+            message=f"server error: {str(e)}",
+            data={}
+        )
+    finally:
+        session.close()
+        
+def remove_bookmark(bookmark_id):
+    session = Session()
+    
+    try:
+        bookmark_to_delete = session.query(Bookmarks).filter(Bookmarks.bookmark_id == bookmark_id).first()
+        session.delete(bookmark_to_delete)
+        session.commit()
+        
+        return api_response(
+            status_code=200,
+            message="Bookmark successfully removed",
+            data={}
+        )
+        
+    except Exception as e:
+        session.rollback()
+        print(f"Error occurred: {e}")
+        return api_response(
+            status_code=500,
+            message=f"server error: {str(e)}",
+            data={}
+        )
+    
+    finally:
+        session.close()
+    
--- app/routes/bookmarks_route.py
+++ app/routes/bookmarks_route.py
@@ -1,7 +1,11 @@
 from flask import Blueprint
 
-from app.controler.bookmark_controler import get_all_bookmarks
+from app.controler.bookmark_controler import get_all_bookmarks, do_bookmark_post, remove_bookmark
 
 bookmarks_routes = Blueprint('bookmarks', __name__)
 
 bookmarks_routes.route("/bookmarks", methods=["GET"])(get_all_bookmarks)
+
+bookmarks_routes.route("/bookmarks/<int:user_id>/<int:post_id>", methods=["POST"])(do_bookmark_post)
+
+bookmarks_routes.route("/bookmarks/<int:bookmark_id>", methods=["DELETE"])(remove_bookmark)
