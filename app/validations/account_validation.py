8d9551e004fd1e58b007fe3cc6dd7d32725ca41d
desyasarbini <desya.sarbini@gmail.com>
2024-08-10 10:49:40 +0800

    feat: add login account in controller, also login validation

--- app/validations/account_validation.py
+++ app/validations/account_validation.py
@@ -1,7 +1,7 @@
-from pydantic import BaseModel, field_validator
+from pydantic import BaseModel, EmailStr, field_validator
 
 class CreateAccount(BaseModel):
-    email: str
+    email: EmailStr
     password: str
     role_id: int
 
@@ -9,6 +9,16 @@ class CreateAccount(BaseModel):
     def email_must_be_provided(cls, v):
         if not v:
             raise ValueError('Email must be provided')
+        return v
+    
+    @field_validator('email')
+    def email_complexity_check(cls, v):
+        if len(v) < 12:
+            raise ValueError('email must be at least 12 characters')
+        if len(v) > 50:
+            raise ValueError('Email must not exceed 50 characters')
+        if '@' not in v or '.' not in v:
+            raise ValueError('Email must contain "@" and "." characters')
         return v
 
     @field_validator('password')
@@ -20,7 +30,7 @@ class CreateAccount(BaseModel):
     @field_validator('password')
     def password_complexity_check(cls, v):
         if len(v) < 8:
-            raise ValueError('Password must be at least 8 characters long')
+            raise ValueError('Password must be at least 8 characters')
         if not any(char.islower() for char in v):
             raise ValueError('Password must contain small character at least one lowercase letter')
         if not any(char.isupper() for char in v):
@@ -34,3 +44,20 @@ class CreateAccount(BaseModel):
         if v <= 0:
             raise ValueError('Role ID must be a positive integer')
         return v
+
+class LoginAccount(BaseModel):
+    email: EmailStr
+    password: str
+
+    @field_validator('email')
+    def email_must_be_provided(cls, v):
+        if not v:
+            raise ValueError('Email must be provided')
+        return v
+
+    @field_validator('password')
+    def password_must_be_provided(cls, v):
+        if not v:
+            raise ValueError('Password must be provided')
+        return v
+    
