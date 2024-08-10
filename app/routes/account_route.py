ab84a2d1825f43079fc5ed350b17a92a41dc85e0
desyasarbini <desya.sarbini@gmail.com>
2024-08-10 10:49:40 +0800

    feat: add login account in controller, also login validation

--- app/routes/account_route.py
+++ app/routes/account_route.py
@@ -1,9 +1,15 @@
 from flask import Blueprint
 
-from app.controler.account_controler import get_all_accounts, create_account
+from app.controler.account_controler import (
+    get_all_accounts,
+    create_account, 
+    login_account
+)
 
 account_routes = Blueprint('account', __name__)
 
 account_routes.route("/account", methods=["GET"])(get_all_accounts)
 
-account_routes.route("account/sigunp", methods=["POST"])(create_account)
+account_routes.route("/account/sigunp", methods=["POST"])(create_account)
+
+account_routes.route("/account/login", methods=["POST"])(login_account)
