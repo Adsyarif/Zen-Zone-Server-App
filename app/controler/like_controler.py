3a6346502f4fab4ae6870db49cbb4b534860f46c
Abk1708 <bgus1708@gmail.com>
2024-08-10 15:20:31 +0700

    feat: add like and bookmark API functionality

--- app/controler/like_controler.py
+++ app/controler/like_controler.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
 from app.models.like import Like
+from app.models.posts import Posts
 from app.connector.sql_connector import Session
 from app.utils.api_response import api_response
 
@@ -13,3 +14,73 @@ def get_all_like():
         return api_response(status_code=500, message=f"Server error: {e}", data={})
     finally:
         session.close()
+        
+def do_like_post(user_id, post_id):
+    session = Session()
+    try:
+        # data = request.json
+        
+        post_query = session.query(Posts).filter(Posts.user_id == user_id)
+        if not post_query:
+            return jsonify({'message': 'No related post found'}), 400
+        
+        like_query = session.query(Like).filter(Like.user_id == user_id, Like.post_id == post_id).first()
+        if like_query:
+            return jsonify({'message': 'Post already liked by the user'}), 400
+        
+        add_like = Like(
+            user_id=user_id,
+            post_id=post_id
+        )
+        
+        # if data.get('')
+        
+        session.add(add_like)
+        session.commit()
+        
+        # data = add_like.serialize()
+        
+        return api_response(
+            status_code=200,
+            message="Post successfully liked",
+            data=add_like.serialize(full=False)
+        )
+    except Exception as e:
+        session.rollback()
+        print(f"Error occurred: {e}")
+        return api_response(
+            status_code=500,
+            message=f"server error: {str(e)}",
+            data={}
+        )
+    finally:
+        session.close()
+        
+def remove_like(like_id):
+    session = Session()
+    
+    try:
+        likes_to_delete = session.query(Like).filter(Like.like_id == like_id).first()
+        session.delete(likes_to_delete)
+        session.commit()
+        
+        return api_response(
+            status_code=200,
+            message="Like successfully removed",
+            data={}
+        )
+        
+    except Exception as e:
+        session.rollback()
+        print(f"Error occurred: {e}")
+        return api_response(
+            status_code=500,
+            message=f"server error: {str(e)}",
+            data={}
+        )
+    
+    finally:
+        session.close()
+    
+# def get_like_by_id(like_id):
+    
--- app/routes/like_route.py
+++ app/routes/like_route.py
@@ -1,7 +1,11 @@
 from flask import Blueprint
 
-from app.controler.like_controler import get_all_like
+from app.controler.like_controler import get_all_like, do_like_post, remove_like
 
 like_routes = Blueprint('like', __name__)
 
 like_routes.route("/like", methods=["GET"])(get_all_like)
+
+like_routes.route("/like/<int:user_id>/<int:post_id>", methods=["POST"])(do_like_post)
+
+like_routes.route("/like/<int:like_id>", methods=["DELETE"])(remove_like)
