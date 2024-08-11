aedf41963601e6f3ff6f1b33b7dd08f8c6131d74
Adrisa <adrisansyarif@gmail.com>
2024-08-11 23:43:53 +0700

    fix: shorted post by like count

--- app/models/posts.py
+++ app/models/posts.py
@@ -2,14 +2,18 @@ from app.models.base import Base
 from sqlalchemy.orm import mapped_column, relationship
 from sqlalchemy import Integer, String, ForeignKey, DateTime, func
 
+
 class Posts(Base):
     __tablename__ = "posts"
 
     post_id = mapped_column(Integer, primary_key=True, autoincrement=True)
-    user_id = mapped_column(Integer, ForeignKey('user_details.user_id', ondelete="CASCADE"))
+    user_id = mapped_column(Integer, ForeignKey(
+        'user_details.user_id', ondelete="CASCADE"))
     content = mapped_column(String)
-    created_at = mapped_column(DateTime(timezone=True), server_default=func.now())
-    deleted_at = mapped_column(DateTime(timezone=True), server_default=func.now())
+    created_at = mapped_column(
+        DateTime(timezone=True), server_default=func.now())
+    deleted_at = mapped_column(
+        DateTime(timezone=True), server_default=func.now())
 
     user_details = relationship("UserDetails", back_populates="posts")
     bookmarks = relationship("Bookmarks", back_populates="posts")
@@ -22,13 +26,14 @@ class Posts(Base):
             'post_id': self.post_id,
             'user_id': self.user_id,
             'content': self.content,
-            'created_at': self.created_at
+            'created_at': self.created_at,
+            'user_name': self.user_details.user_name,
         }
         if full:
             data.update({
                 'deleted_at': self.deleted_at
             })
         return data
-    
+
     def __repr__(self):
         return f'<Posts {self.post_id}>'
--- app/routes/post_route.py
+++ app/routes/post_route.py
@@ -1,7 +1,11 @@
 from flask import Blueprint
 
-from app.controler.post_controler import get_all_post
+from app.controler.post_controler import (
+    get_all_post,
+    get_post_by_id
+)
 
 post_routes = Blueprint('post', __name__)
 
 post_routes.route("/post", methods=["GET"])(get_all_post)
+post_routes.route("/post/<int:post_id>", methods=["GET"])(get_post_by_id)
