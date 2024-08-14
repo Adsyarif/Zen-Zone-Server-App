7372824ee9cc22f626dc28561cb00f4d005aefb3
MEIRTH MAULIDA H <meirthmh.mei@gmail.com>
2024-08-14 21:31:10 +0900

    feat: merge dairy_api to main branch

--- app/routes/diary_route.py
+++ app/routes/diary_route.py
@@ -1,7 +1,11 @@
 from flask import Blueprint
 
-from app.controler.diary_controler import get_all_diary
+from app.controler.diary_controler import get_all_diary, get_diary_by_id, create_diary_entry, edit_diary_by_id, soft_delete_diary_entry_by_id
 
 diary_routes = Blueprint('diary', __name__)
 
 diary_routes.route("/diary", methods=["GET"])(get_all_diary)
+diary_routes.route("/diary/<int:account_id>/<int:diary_id>", methods=["GET"])(get_diary_by_id)
+diary_routes.route("/diary/<int:account_id>/create", methods=["POST"])(create_diary_entry)
+diary_routes.route("/diary/<int:account_id>/<int:diary_id>/edit", methods=["PUT"])(edit_diary_by_id)
+diary_routes.route("/diary/<int:account_id>/<int:diary_id>/delete", methods=["DELETE"])(soft_delete_diary_entry_by_id)
