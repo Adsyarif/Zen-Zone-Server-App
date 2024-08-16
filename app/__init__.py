56f03758f5e0962c634051c3239e8a1af8d97f6f
desyasarbini <desya.sarbini@gmail.com>
2024-08-17 01:24:46 +0800

    fix: edit cors

--- app/__init__.py
+++ app/__init__.py
@@ -28,8 +28,7 @@ app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
 jwt = JWTManager(app)
 
-CORS(app, origins=['http://localhost:5000'
-], supports_credentials=True)
+CORS(app, origins=['*'], supports_credentials=True)
 
 @app.route("/")
 def hello_world():
