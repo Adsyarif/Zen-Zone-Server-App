5cde05f6f5a8db4bd4ef1823b1c7c7777647cfee
desyasarbini <desya.sarbini@gmail.com>
2024-08-16 23:10:45 +0800

    feat: revisi docker and port

--- app/__init__.py
+++ app/__init__.py
@@ -28,7 +28,7 @@ app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
 jwt = JWTManager(app)
 
-CORS(app, origins=['http://localhost:3000'
+CORS(app, origins=['http://localhost:5000'
 ], supports_credentials=True)
 
 @app.route("/")
