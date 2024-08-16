fbc2c6f65e388e768bd84911f30aac6ec2421bcc
desyasarbini <desya.sarbini@gmail.com>
2024-08-16 23:41:36 +0800

    feat: edit connector

--- app/connector/sql_connector.py
+++ app/connector/sql_connector.py
@@ -11,9 +11,7 @@ DATABASE_PASSWORD = os.getenv('PASSWORD')
 
 print('Connecting to SQL Server')
 
-connection_string = f'{DATABASE_TYPE}://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}'
-
-engine = create_engine(connection_string)
+engine = create_engine(f'{DATABASE_TYPE}://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}')
 
 connection = engine.connect()
 Session = sessionmaker(connection)
