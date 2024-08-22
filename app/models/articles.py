47200f7147e5a9b8ad0d0d18063d53b9cb94e9f0
Orang9 <egbert.fewi@gmail.com>
2024-08-23 00:34:47 +0700

    feat: articles model

---
+++ app/models/articles.py
@@ -0,0 +1,31 @@
+from app.models.base import Base
+from sqlalchemy.orm import mapped_column, relationship
+from sqlalchemy import String, Integer, DateTime, func
+
+
+class Articles(Base):
+    __tablename__ = "articles"
+
+    article_id = mapped_column(Integer, primary_key=True, autoincrement=True)
+    title = mapped_column(String, nullable=False)
+    author = mapped_column(String, nullable=False)
+    summary = mapped_column(String, nullable=False)
+    tag = mapped_column(String, nullable=False)
+    created_at = mapped_column(DateTime(timezone=True), server_default=func.now())
+    deleted_at = mapped_column(DateTime(timezone=True), nullable=True)
+
+    article_content = relationship("ArticleContent", back_populates="articles")
+
+    def serialize(self):
+        return {
+            'article_id': self.article_id,
+            'title': self.title,
+            'author': self.author,
+            'summary': self.summary,
+            'tag': self.tag,
+            'created_at': self.created_at,
+            'deleted_at': self.deleted_at
+        }
+    
+    def __repr__(self):
+        return f'<Article {self.article_id}>'
