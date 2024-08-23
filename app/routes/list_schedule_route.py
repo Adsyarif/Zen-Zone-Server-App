e3ee9816f50132970e72c7a62a4a1a9813b0f92f
widiapuspitasar <puspitasariwidia@gmail.com>
2024-08-23 18:02:14 +0700

    feat:add review counselor

--- app/routes/list_schedule_route.py
+++ app/routes/list_schedule_route.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from app.controler.list_schedule_controller import get_all_list_schedules
 
-list_schedule = Blueprint('list_schedule', __name__)
+list_scedule_routes = Blueprint('list_schedule', __name__)
 
-list_schedule.route("/list_schedule", methods=["GET"])(get_all_list_schedules)
+list_scedule_routes.route("/list_schedule", methods=["GET"])(get_all_list_schedules)
 
