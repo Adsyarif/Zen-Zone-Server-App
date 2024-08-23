ed1bbaf8a78980bf5f852b72263d12e9f1cdc21a
Orang9 <egbert.fewi@gmail.com>
2024-08-23 17:22:42 +0700

    feat: PUT route in Articles route

--- app/routes/articles_route.py
+++ app/routes/articles_route.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from app.controler.articles_controler import get_all_articles, get_articles_by_article_id, get_articles_by_tag, create_article, delete_article
+from app.controler.articles_controler import get_all_articles, get_articles_by_article_id, get_articles_by_tag, create_article, delete_article, edit_article
 
 articles_routes = Blueprint('articles', __name__)
 
@@ -7,5 +7,6 @@ articles_routes.route("/articles", methods=["GET"])(get_all_articles)
 articles_routes.route("/articles/<int:article_id>", methods=["GET"])(get_articles_by_article_id)
 articles_routes.route("/articles/tag/<string:tag>", methods=["GET"])(get_articles_by_tag)
 articles_routes.route("/articles/create", methods=["POST"])(create_article)
+articles_routes.route("/articles/<int:article_id>", methods=["PUT"])(edit_article)
 articles_routes.route("/articles/<int:article_id>", methods=["DELETE"])(delete_article)
 
