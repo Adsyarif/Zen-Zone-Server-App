f6d23c5c932e039efc1924bd74bf8e945d84c99c
Orang9 <egbert.fewi@gmail.com>
2024-08-24 19:02:42 +0700

    refactor: create and edit article content

--- app/controler/article_content_controler.py
+++ app/controler/article_content_controler.py
@@ -4,6 +4,18 @@ from app.connector.sql_connector import Session
 from app.utils.api_response import api_response
 from app.models.articles import Articles
 
+def get_all_article_content():
+    session = Session()
+    
+    try:
+        article_contents = session.query(ArticleContent).all()
+        data = [article_content.serialize() for article_content in article_contents]
+        return api_response(status_code=200, message="article contents retrieved successfully", data=data)
+    except Exception as e:
+        return api_response(status_code=500, message=f"Server error: {e}", data={})
+    finally:
+        session.close()
+
 def get_artilce_content_by_article_id(article_id):
     session = Session()
     try:
@@ -45,6 +57,7 @@ def create_article_content(article_id):
 
         session.add(new_article_content)
         session.commit()
+        session.refresh(new_article_content)
         return api_response(status_code=201, message="Article created successfully", data=new_article_content.serialize())
     
     except Exception as e:
@@ -68,8 +81,11 @@ def edit_article_content(article_content_id):
             article_content_to_edit.sub_bab = data['sub_bab']
         if 'paragraph' in data:
             article_content_to_edit.author = data['paragraph']
+        if 'updated_at' in data:
+            article_content_to_edit.author = data['updated_at']
 
         session.commit()
+        session.refresh(article_content_to_edit)
         return api_response(status_code=200, message="Article content updated successfully", data=article_content_to_edit.serialize())
     
     except Exception as e:
