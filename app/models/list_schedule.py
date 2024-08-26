24c595522596d944849ce65bcdf7c284ca6c3db9
widiapuspitasar <puspitasariwidia@gmail.com>
2024-08-27 01:16:34 +0700

    feat:edit database shedules

--- app/models/list_schedule.py
+++ app/models/list_schedule.py
@@ -1,6 +1,6 @@
-from app.models.base import  Base
+from app.models.base import Base
 from sqlalchemy.orm import mapped_column, relationship
-from sqlalchemy import Integer, ForeignKey, DateTime, String , func
+from sqlalchemy import Integer, ForeignKey, DateTime, String, func
 
 class ListSchedule(Base):
     __tablename__ = "list_schedule"
@@ -8,14 +8,15 @@ class ListSchedule(Base):
     schedule_id = mapped_column(Integer, primary_key=True, autoincrement=True)
     available_from = mapped_column(DateTime(timezone=True), nullable=False)
     available_to = mapped_column(DateTime(timezone=True), nullable=False)
-    booked_by_account_id = mapped_column(Integer, ForeignKey('user_details.user_id', ondelete="CASCADE"))
-    counselor_id = mapped_column(Integer, ForeignKey('counselor_details.counselor_id', ondelete="CASCADE"))
+    booked_by_account_id = mapped_column(Integer, ForeignKey('account.account_id', ondelete="CASCADE"))
+    counselor_id = mapped_column(Integer, ForeignKey('account.account_id', ondelete="CASCADE"))
     created_at = mapped_column(DateTime(timezone=True), server_default=func.now())
     updated_at = mapped_column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
     deleted_at = mapped_column(DateTime(timezone=True), server_default=func.now())
     status = mapped_column(String, nullable=True)
-    user_details = relationship("UserDetails", back_populates="list_schedule")
-    counselor_details = relationship("CounselorDetail", back_populates="list_schedules")
+
+    booked_by_account = relationship("Account", back_populates="booked_schedules", foreign_keys=[booked_by_account_id])
+    counselor = relationship("Account", back_populates="counselor_schedules", foreign_keys=[counselor_id])
 
     def serialize(self, full=True):
         data = {
@@ -25,14 +26,15 @@ class ListSchedule(Base):
             'booked_by_account_id': self.booked_by_account_id,
             'counselor_id': self.counselor_id,
             'created_at': self.created_at,
-            'status': self.status
+            'status': self.status,
         } 
         if full:
             data.update({
                 'updated_at': self.updated_at,
                 'deleted_at': self.deleted_at
             })
+        
         return data
     
     def __repr__(self):
-        return f'<ListSchedule{self.schedule_id}>'
+        return f'<ListSchedule {self.schedule_id}>'
