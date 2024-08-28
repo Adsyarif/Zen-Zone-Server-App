c6d19c2439f0c5ae3786c4a5a69ecc6a23861beb
widiapuspitasar <puspitasariwidia@gmail.com>
2024-08-28 14:40:42 +0700

    feat: add controler mark schedules

--- app/models/list_schedule.py
+++ app/models/list_schedule.py
@@ -1,7 +1,9 @@
 from app.models.base import Base
 from sqlalchemy.orm import mapped_column, relationship
 from sqlalchemy import Integer, ForeignKey, DateTime, String, func
+from datetime import timezone, timedelta
 
+JAKARTA_TZ = timezone(timedelta(hours=7))
 class ListSchedule(Base):
     __tablename__ = "list_schedule"
 
@@ -21,8 +23,8 @@ class ListSchedule(Base):
     def serialize(self, full=True):
         data = {
             'schedule_id': self.schedule_id,
-            'available_from': self.available_from,
-            'available_to': self.available_to,
+            'available_from': self.available_from.astimezone(JAKARTA_TZ).strftime('%Y-%m-%d %H:%M:%S') if self.available_from else None,
+            'available_to': self.available_to.astimezone(JAKARTA_TZ).strftime('%Y-%m-%d %H:%M:%S') if self.available_to else None,
             'booked_by_account_id': self.booked_by_account_id,
             'counselor_id': self.counselor_id,
             'created_at': self.created_at,
--- app/routes/list_schedule_route.py
+++ app/routes/list_schedule_route.py
@@ -8,7 +8,8 @@ from app.controler.list_schedule_controller import (
     put_cancel_by_account_id, 
     put_update_status_by_counselor_id , 
     delete_schedule_by_counselor_id,
-    get_schedule_by_booked_by_account_id
+    get_schedule_by_booked_by_account_id,
+    mark_schedule_as_done
     )
 
 list_scedule_routes = Blueprint('list_schedule', __name__)
@@ -22,3 +23,4 @@ list_scedule_routes.route("/list_schedule/reschedule/<int:counselor_id>/<int:sch
 list_scedule_routes.route("/list_schedule/counselor/delete/<int:counselor_id>/<int:schedule_id>", methods=["DELETE"])(delete_schedule_by_counselor_id)
 
 list_scedule_routes.route("/list_schedule/user/<int:booked_by_account_id>/", methods=["GET"])(get_schedule_by_booked_by_account_id)
+list_scedule_routes.route("/list_schedule/status/<int:counselor_id>/<int:schedule_id>", methods=["PUT"])(mark_schedule_as_done)
