20fa09ff7c68ef810682afbdb0510a776f3a49e5
muhammad umar tsaqib <tsaqib@MacBook-Pro-muhammad.local>
2024-08-30 09:20:53 +0700

    new feedback.py

---
+++ app/models/feedback.py
@@ -0,0 +1,36 @@
+from app.models.base import Base
+from sqlalchemy.orm import mapped_column, relationship
+from sqlalchemy import String, Integer, ForeignKey, DateTime, func, Float
+
+from datetime import timezone, timedelta
+
+JAKARTA_TZ = timezone(timedelta(hours=7))
+class Feedback(Base):
+    __tablename__ = "feedback"
+
+    feedback_id = mapped_column(Integer, primary_key=True, autoincrement=True)
+    account_id = mapped_column(Integer, ForeignKey('account.account_id', ondelete="CASCADE"))
+    description = mapped_column(String)
+    rating = mapped_column(Float)
+    created_at = mapped_column(DateTime(timezone=True), server_default=func.now())
+    updated_at = mapped_column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+    deleted_at = mapped_column(DateTime(timezone=True), server_default=func.now())
+
+    def serialize(self, full=True):
+        data = {
+            'feedback_id': self.feedback_id,
+            'account_id': self.account_id, 
+            'content': self.content,
+            'description': self.description,
+            'rating': self.rating,
+            'created_at': self.created_at.astimezone(JAKARTA_TZ).strftime('%Y-%m-%d %H:%M:%S') if self.created_at else None
+        }
+        if full:
+            data.update ({
+                'updated_at': self.updated_at.astimezone(JAKARTA_TZ).strftime('%Y-%m-%d %H:%M:%S') if self.updated_at else None,
+                'deleted_at': self.deleted_at.astimezone(JAKARTA_TZ).strftime('%Y-%m-%d %H:%M:%S') if self.deleted_at else None
+            })
+        return data
+    
+    def __repr__(self):
+        return f'<Feedback{self.feedback_id}>'
---
+++ app/routes/feedback_route.py
@@ -0,0 +1,19 @@
+from flask import Blueprint
+
+from app.controler.diary_controler import (
+    get_all_feedback, 
+    get_feedback_by_id, 
+    create_feedback_entry, 
+    edit_feedback_by_id, 
+    soft_delete_feedback_entry_by_id, 
+    get_feedback_by_account_id
+    )
+
+feedback_routes = Blueprint('feedback', __name__)
+
+feedback_routes.route("/feedback", methods=["GET"])(get_all_feedback)
+feedback_routes.route("/feedback/<int:account_id>/<int:diary_id>", methods=["GET"])(get_feedback_by_id)
+feedback_routes.route("/feedback/<int:account_id>/create", methods=["POST"])(create_feedback_entry)
+feedback_routes.route("/feedback/<int:account_id>/<int:diary_id>/edit", methods=["PUT"])(edit_feedback_by_id)
+feedback_routes.route("/feedback/<int:account_id>/<int:diary_id>/delete", methods=["DELETE"])(soft_delete_feedback_entry_by_id)
+feedback_routes.route("/feedback/<int:account_id>", methods=["GET"])(get_feedback_by_account_id)
